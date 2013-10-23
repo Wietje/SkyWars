efb86dc06523a40883a952b181e112e5d7a59141
Wietje <wietje24@live.com>
2013-10-23 22:58:56 +0200

    Fix messaging in start command

--- src/main/java/vc/pvp/skywars/commands/StartCommand.java
+++ src/main/java/vc/pvp/skywars/commands/StartCommand.java
@@ -19,13 +19,13 @@ public class StartCommand implements CommandExecutor {
         GamePlayer gamePlayer = PlayerController.get().get((Player) sender);
 
         if (!gamePlayer.isPlaying()) {
-            sender.sendMessage(new Messaging.MessageFormatter().format("not-in-game"));
+            sender.sendMessage(new Messaging.MessageFormatter().format("error.not-in-game"));
         } else if (gamePlayer.getGame().getState() != GameState.WAITING) {
-            sender.sendMessage(new Messaging.MessageFormatter().format("already-started"));
+            sender.sendMessage(new Messaging.MessageFormatter().format("error.already-started"));
         } else if (gamePlayer.getGame().getPlayerCount() < 2) {
-            sender.sendMessage(new Messaging.MessageFormatter().format("not-enough-players"));
+            sender.sendMessage(new Messaging.MessageFormatter().format("error.not-enough-players"));
         } else if (PluginConfig.buildSchematic() && !gamePlayer.getGame().isBuilt()) {
-            sender.sendMessage(new Messaging.MessageFormatter().format("arena-under-construction"));
+            sender.sendMessage(new Messaging.MessageFormatter().format("error.arena-under-construction"));
         } else {
             gamePlayer.getGame().onGameStart();
         }
